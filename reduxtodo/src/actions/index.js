84f4a714132d712803b4903d801911616f262891
Tushar8237 <87122609+Tushar8237@users.noreply.github.com>
2022-05-07 03:29:49 +0530

    DotoList

---
+++ reduxtodo/src/actions/index.js
@@ -0,0 +1,17 @@
+export const addTodo = () => {
+    return {
+        type: "ADD_TODO"
+    }
+}
+
+export const deleleTodo = () => {
+    return {
+        type: "DELETE_TODO"
+    }
+}
+
+export const removeTodo = () => {
+    return {
+        type: "REMOVE_TODO"
+    }
+}
---
+++ reduxtodo/src/component/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import "./todo.css";
+
+const Todo = () => {
+
+    const [inputData, setInputData] = useState ('');
+  return (
+    <div>
+       <div className='main-div'>
+      <div className='child-div'>
+        <figure>
+        <figcaption>Add Your List Here ✌️ </figcaption>
+        </figure>
+
+        <div className='addItem'>
+          <input type="text" placeholder=' ✍️ Add Items.. '
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+          />
+          <i className='fa fa-plus add-btn' ></i>
+        </div>
+      </div>
+     
+    </div>
+    </div>
+  )
+}
+
+export default Todo
